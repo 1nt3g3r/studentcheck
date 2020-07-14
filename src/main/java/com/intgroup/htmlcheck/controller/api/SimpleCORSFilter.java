3d398d00fb5d937acd3ff48d7e38b3e0ee5863eb
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2020-07-14 17:04:05 +0300

    Fixed CORS filter

--- src/main/java/com/intgroup/htmlcheck/controller/api/SimpleCORSFilter.java
+++ src/main/java/com/intgroup/htmlcheck/controller/api/SimpleCORSFilter.java
@@ -19,15 +19,20 @@ import org.springframework.stereotype.Component;
 public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		System.out.println("Do filter");
 		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+		
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		response.setHeader("Access-Control-Expose-Headers", "User-Cookie");
 
-		chain.doFilter(req, res);
+		if ("OPTIONS".equals(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, response);
+		}
 	}
 
 	public void init(FilterConfig filterConfig) {}
